332f0cc11421538370631514a1560dcaed282164
ShadrackAdwera <adweshshaddie@gmail.com>
2023-08-01 10:28:28 +0300

    fix db name in insert query

--- internal/db/query/user.sql
+++ internal/db/query/user.sql
@@ -4,17 +4,17 @@ VALUES ($1,$2,$3, $4)
 RETURNING *;
 
 -- name: ListUsers :many
-SELECT * FROM user 
+SELECT * FROM users 
 ORDER BY id
 LIMIT $1 
 OFFSET $2;
 
 -- name: GetUser :one
-SELECT * FROM user
+SELECT * FROM users
 WHERE id = $1;
 
 -- name: UpdateUser :one
-UPDATE user
+UPDATE users
 SET
   username = COALESCE(sqlc.narg(username),username),
   email = COALESCE(sqlc.narg(email),email),
@@ -24,5 +24,5 @@ WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteUser :exec
-DELETE FROM user
+DELETE FROM users
 WHERE id = $1;
