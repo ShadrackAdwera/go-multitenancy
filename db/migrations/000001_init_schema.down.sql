f09a6c8a1e1b4673a8ebb96796a4ae34982b7afd
ShadrackAdwera <adweshshaddie@gmail.com>
2023-08-01 10:28:56 +0300

    modify table name for users - rewrite migrate down sql

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,27 +1,28 @@
--- Drop foreign keys first
-ALTER TABLE "user_group" DROP CONSTRAINT "user_group_group_id_fkey";
-ALTER TABLE "user_group" DROP CONSTRAINT "user_group_user_id_fkey";
-ALTER TABLE "group_policy" DROP CONSTRAINT "group_policy_policy_id_fkey";
-ALTER TABLE "group_policy" DROP CONSTRAINT "group_policy_group_id_fkey";
-ALTER TABLE "profile" DROP CONSTRAINT "profile_group_id_fkey";
-ALTER TABLE "profile" DROP CONSTRAINT "profile_user_id_fkey";
-ALTER TABLE "permission" DROP CONSTRAINT "permission_policy_id_fkey";
-ALTER TABLE "policy" DROP CONSTRAINT "policy_group_id_fkey";
-ALTER TABLE "user" DROP CONSTRAINT "user_tenant_id_fkey";
+-- Drop foreign key constraints
+ALTER TABLE "profile" DROP CONSTRAINT IF EXISTS "profile_user_id_fkey";
+ALTER TABLE "profile" DROP CONSTRAINT IF EXISTS "profile_group_id_fkey";
+ALTER TABLE "policy" DROP CONSTRAINT IF EXISTS "policy_group_id_fkey";
+ALTER TABLE "permission" DROP CONSTRAINT IF EXISTS "permission_policy_id_fkey";
+ALTER TABLE "users_group" DROP CONSTRAINT IF EXISTS "users_group_user_id_fkey";
+ALTER TABLE "users_group" DROP CONSTRAINT IF EXISTS "users_group_group_id_fkey";
+ALTER TABLE "users" DROP CONSTRAINT IF EXISTS "users_tenant_id_fkey";
+ALTER TABLE "group_policy" DROP CONSTRAINT IF EXISTS "group_policy_group_id_fkey";
+ALTER TABLE "group_policy" DROP CONSTRAINT IF EXISTS "group_policy_policy_id_fkey";
 
--- Drop indexes
-DROP INDEX "user_group_group_id_user_id_idx";
-DROP INDEX "group_policy_group_id_policy_id_idx";
-DROP INDEX "profile_id_user_id_idx";
-DROP INDEX "permission_id_policy_id_idx";
-DROP INDEX "user_id_tenant_id_idx";
+-- Drop unique indexes
+DROP INDEX IF EXISTS "users_id_tenant_id_idx";
+DROP INDEX IF EXISTS "permission_id_policy_id_idx";
+DROP INDEX IF EXISTS "profile_id_user_id_idx";
+DROP INDEX IF EXISTS "group_policy_group_id_policy_id_idx";
+DROP INDEX IF EXISTS "users_group_group_id_user_id_idx";
 
--- Drop tables in reverse order
-DROP TABLE "user_group";
-DROP TABLE "group_policy";
-DROP TABLE "profile";
-DROP TABLE "permission";
-DROP TABLE "policy";
-DROP TABLE "group";
-DROP TABLE "user";
-DROP TABLE "tenant";
+-- Drop tables
+DROP TABLE IF EXISTS "users_group";
+DROP TABLE IF EXISTS "group_policy";
+DROP TABLE IF EXISTS "group";
+DROP TABLE IF EXISTS "profile";
+DROP TABLE IF EXISTS "permission";
+DROP TABLE IF EXISTS "policy";
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "tenant";
+
